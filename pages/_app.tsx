289e1d36df3a588de4ce91a3ac911ae8acf147ad
Albano Xhafaj <albano.xhafaj10@gmail.com>
2022-03-18 18:50:38 +0400

    Refactor: App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../components';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
-export default MyApp
+export default App;
